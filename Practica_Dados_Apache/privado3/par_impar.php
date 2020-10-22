9c8e698f22dbdc9703884df9aa73499d4c20ef8f
cgr <cristinagrechedaw@gmail.com>
2020-10-22 15:12:19 +0200

    inclusión de carpeta con archivos

---
+++ Practica_Dados_Apache/privado3/par_impar.php
@@ -0,0 +1,7 @@
+<?php
+//session_start(); NO hace falta ya la abrimos en menu.php
+include_once '../menu/menu.php';
+if(!isset($_SESSION['logueado']))
+{
+    header('Location:../index.php');//redirigir a otra pagina
+}
